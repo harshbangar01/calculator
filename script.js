a5a7a8ac6548bf632060da06e1a25c3cc3d28176
harshbangar01 <harshbang098@gmail.com>
2025-04-15 11:57:08 +0530

    bug fixed

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function handleSymbol(symbol) {
             break;
         case '+':
         case '-':
-        case 'x':
+        case '×': 
         case '÷':
             handleMath(symbol);
             break;
@@ -67,7 +67,7 @@ function flushOperation(intBuffer) {
         runningTotal += intBuffer;
     } else if (previousOperator === '-') {
         runningTotal -= intBuffer;
-    } else if (previousOperator === 'x') {
+    } else if (previousOperator === '×') {
         runningTotal *= intBuffer;
     } else if (previousOperator === '÷') {
         runningTotal /= intBuffer;
